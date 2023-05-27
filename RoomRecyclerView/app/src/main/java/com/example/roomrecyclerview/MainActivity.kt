2c300abffc70d4c504694ff52adeebcb8455d4b0
Hyunji Um <umhj0807@khu.ac.kr>
2023-05-28 02:13:41 +0900

    Connect Link

---
+++ RoomRecyclerView/app/src/main/java/com/example/roomrecyclerview/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.roomrecyclerview
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomrecyclerview.databinding.ActivityMainBinding
+import com.example.roomrecyclerview.databinding.CardItemUserViewHolderBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var job: Job? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = UserAdapter()
+
+        binding.btnSearch.setOnClickListener {
+            read()
+        }
+    }
+
+
+    private fun read() {
+        job = lifecycleScope.launch() {
+            withContext(Dispatchers.IO) {
+                val searchword = binding.editText.text.toString()
+                val userList = AppDatabase(this@MainActivity).studentDao().findByRoll(searchword)
+                runOnUiThread {
+                    binding.recyclerView.apply {
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        adapter = UserAdapter().apply {
+                            setData(userList)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RoomRecyclerView/app/src/main/java/com/example/roomrecyclerview/StudentDao.kt
@@ -0,0 +1,18 @@
+package com.example.roomrecyclerview
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface StudentDao {
+
+    @Query("SELECT * FROM total_table ORDER BY id DESC")
+    fun getAllUser(): List<Student>
+
+    @Query("SELECT * FROM total_table WHERE menu_name LIKE '%' || :roll || '%' ORDER BY avr_star DESC")
+    fun findByRoll(roll: String): List<Student>
+}
